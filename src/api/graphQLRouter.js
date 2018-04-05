84995b637e6bd7473523bce75fe8e48ecbe00b6a
joaop-oliveira <jpaulo2212gt@gmail.com>
2018-04-04 22:57:40 -0300

    Fixed the Document query population issue updating the authentication next

--- src/api/graphQLRouter.js
+++ src/api/graphQLRouter.js
@@ -1,7 +1,8 @@
-import { makeExecutableSchema } from "graphql-tools";
-import { userType, userResolvers } from "./resources/user";
-import merge from "lodash.merge";
-import { graphqlExpress } from "apollo-server-express";
+import { makeExecutableSchema } from 'graphql-tools';
+import { userType, userResolvers } from './resources/user';
+import { clanTypes, clanResolvers } from './resources/clan/';
+import merge from 'lodash.merge';
+import { graphqlExpress } from 'apollo-server-express';
 
 const baseSchema = `
   schema { 
@@ -11,14 +12,14 @@ const baseSchema = `
 `;
 
 const schema = makeExecutableSchema({
-  typeDefs: [baseSchema, userType],
-  resolvers: merge({}, userResolvers),
+  typeDefs: [baseSchema, userType, clanTypes],
+  resolvers: merge({}, userResolvers, clanResolvers)
 });
 
 export const graphQLRouter = graphqlExpress(req => ({
   schema,
   context: {
     req,
-    user: req.user,
-  },
+    user: req.user
+  }
 }));
