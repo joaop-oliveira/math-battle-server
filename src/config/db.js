abb57bf0f77c603bbc754eb1c22adcdbf29a7e71
joaop-oliveira <jpaulo2212gt@gmail.com>
2018-04-18 20:34:04 -0300

    change the db config settings

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 mongoose.Promise = global.Promise;
 
 export const connect = () => {
-    return mongoose.connect("mongodb://joaop-oliveira:221294gt@ds051524.mlab.com:51524/math-battle-db");
+    return mongoose.connect("mongodb://math-battle-user:221294gt@ds147589.mlab.com:47589/math-battle-db");
 };
 // return mongoose.connect("mongodb://joaop.oliveira:221294gt@ds051524.mlab.com:51524/math-battle-db");
 
